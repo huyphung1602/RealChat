3e2d6219fea23d0a6435bb0147ebbbc27a6c4433
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-13 16:50:31 +0700

    Add reload function after 3s, add message index(html, js), add respond_to format.js

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,21 +6,34 @@ class MessagesController < ApplicationController
     respond_to do |format|
       format.html
       format.js
+      format.json do
+        render json: @messages
+      end
     end
   end
 
   def create
     set_room
+    # sleep 2
     @message = @room.messages.build message_params
 
     if @message.save
       unless current_user
         session[:current_user] = @message.username
       end
-      redirect_to @room
-    else
-      flash[:error] = "Error #{@message.errors.full_messages.to_sentence}"
-      redirect_back fallback_location: room_path(@room)
+    end
+
+    respond_to do |format|
+      format.html do
+        if @message.persisted?
+          redirect_to @room
+        else
+          flash[:error] = "Error #{@message.errors.full_messages.to_sentence}"
+          redirect_back fallback_location: room_path(@room)
+        end
+      end
+
+      format.js
     end
   end
 
